9ec38d68b02663b4f1ac184fcc1d28abec765998
Joakim Eriksson <75730728+ErikssonJoakim@users.noreply.github.com>
2022-04-14 15:30:32 +0200

    refactor: update return method

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,5 @@
 export const getRandomNumber = (minValue: number, maxValue: number): number => {
     const num: number = Math.floor(Math.random() * (maxValue - minValue) + minValue)
     if (num === 2) { return num + 1 }
-    else { return num }
+   return num === 2 ? num + 1 : num
 }
