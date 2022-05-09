baad567d532608a6dcabb5339aaa0f3578b02c26
ErikssonJoakim <j.eriksson@okp4.com>
2022-05-09 12:01:08 +0200

    feat: add redux calls to functions

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,38 +1,22 @@
-import React, { useState, useCallback } from 'react'
+import React, { useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Header from '../components/header/Header'
 import Counter from '../components/counter/Counter'
 import Button from '../components/button/Button'
 import './app.scss'
-import { getRandomNumber } from '../utils'
+import type { AppState } from '../store/appState'
 
+export const minValue: number = 2
+export const maxValue: number = 8
 const header: string = 'Counter App'
 const description: string =
   'This application creates a random number and allows the user to increment or decrement the number by 1.'
-const minValue: number = 2
-const maxValue: number = 8
 
 const App = (): JSX.Element => {
-  const [value, setValue]: readonly [number, (newValue: number) => void] = useState(
-    getRandomNumber(minValue, maxValue)
-  )
+  const dispatch = useDispatch()
+  const value = useSelector((state: AppState) => state.value)
 
-  const handleIncrement = useCallback((): void => {
-    if (value < maxValue) {
-      setValue(value + 1)
-    }
-  }, [value, setValue])
-
-  const handleDecrement = useCallback((): void => {
-    if (value > minValue) {
-      setValue(value - 1)
-    }
-  }, [value, setValue])
-
-  const handleReset = useCallback((): void => {
-    setValue(0)
-  }, [setValue])
-
-  const displayMessage = useCallback((): string | undefined => {
+  const displayMessage = (): string | undefined => {
     switch (value) {
       case maxValue:
         return `Value is max: ${value}`
@@ -41,7 +25,23 @@ const App = (): JSX.Element => {
       default:
         return
     }
-  }, [value])
+  }
+
+  const handleIncrement = useCallback((): void => {
+    if (value < maxValue) {
+      dispatch({ type: 'counter/incremented' })
+    }
+  }, [value, dispatch])
+
+  const handleDecrement = useCallback((): void => {
+    if (value > minValue) {
+      dispatch({ type: 'counter/decremented' })
+    }
+  }, [value, dispatch])
+
+  const handleReset = useCallback((): void => {
+    dispatch({ type: 'counter/reseted' })
+  }, [dispatch])
 
   return (
     <div className="react-project-main">
