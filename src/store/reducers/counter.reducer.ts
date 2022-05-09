9dace1cbb368cd95f5e312a64f7f7bba7b380a9a
ErikssonJoakim <j.eriksson@okp4.com>
2022-05-09 14:20:33 +0200

    refactor: remove unnecessary spread of state

--- src/store/reducers/counter.reducer.ts
+++ src/store/reducers/counter.reducer.ts
@@ -10,11 +10,11 @@ const initialState: AppState = {
 const counterReducer = (state: AppState = initialState, action: Readonly<Action>): AppState => {
   switch (action.type) {
     case 'counter/incremented':
-      return { ...state, value: state.value + 1 }
+      return { value: state.value + 1 }
     case 'counter/decremented':
-      return { ...state, value: state.value - 1 }
+      return { value: state.value - 1 }
     case 'counter/reseted':
-      return { ...state, value: 0 }
+      return { value: 0 }
     default:
       return state
   }
