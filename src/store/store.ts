6f29b52bbb961ea0e429a207fea2a9363bf462ce
ErikssonJoakim <j.eriksson@okp4.com>
2022-05-09 11:58:52 +0200

    build: add redux devtools dependency

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
 import type { Store, Action } from 'redux'
-import { createStore } from 'redux'
+import { legacy_createStore as createStore } from 'redux'
 import type { AppState } from './appState'
 import counterReducer from './reducers/counter.reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store: Store<AppState, Action<string>> = createStore(counterReducer)
+const store: Store<AppState, Action<string>> = createStore(counterReducer, composeWithDevTools())
 
 export default store
